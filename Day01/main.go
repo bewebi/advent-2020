7f69c1ad0e76ea2858b6162a11ad6307eaec6e31
Bernie Birnbaum <bbirnbaum@cogolabs.com>
2020-12-03 14:37:11 -0500

    refactor

--- Day01/main.go
+++ Day01/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 )
 
 var (
-	target = flag.Int64("target", 2020, "target sum")
+	target   = flag.Int64("target", 2020, "target sum")
+	threeSum = flag.Bool("threeSum", false, "find three input lines that sum to target rather than two")
 )
 
 func main() {
@@ -22,19 +24,37 @@ func main() {
 	}
 	defer f.Close()
 
+	s := bufio.NewScanner(f)
+
+	nums, err := twoSum(s)
+	if err != nil {
+		log.Fatal(err)
+	} else if nums == nil {
+		log.Print("no solution found")
+	}
+	log.Printf("Inputs %v sum to %d! Their product is %d", nums, *target, product(nums))
+}
+
+func twoSum(s *bufio.Scanner) ([]int64, error) {
 	encountered := map[int64]bool{}
 
-	s := bufio.NewScanner(f)
 	for s.Scan() {
 		i, err := strconv.ParseInt(s.Text(), 10, 64)
 		if err != nil {
-			log.Fatal("error parsing input as int64")
+			return nil, fmt.Errorf("error parsing input as int64: %v, err")
 		}
 		if encountered[*target-i] {
-			log.Printf("Inputs %d and %d sum to %d! Their product is %d", *target-i, i, *target, (*target-i)*i)
-			return
+			return []int64{*target - i, i}, nil
 		}
 		encountered[i] = true
 	}
-	log.Print("no solution found")
+	return nil, nil
+}
+
+func product(nums []int64) int64 {
+	ret := int64(1)
+	for _, i := range nums {
+		ret *= i
+	}
+	return ret
 }
