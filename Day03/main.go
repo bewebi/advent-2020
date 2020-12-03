2be2305396372e0ba5d561079b462fe54c8884f3
Bernie Birnbaum <bbirnbaum@cogolabs.com>
2020-12-03 18:52:53 -0500

    Day 3 part 2

--- Day03/main.go
+++ Day03/main.go
@@ -6,31 +6,53 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type row []bool
 type field []row
 
+type slope struct {
+	x, y int64
+}
+
 var (
-	flagSlope = flag.Int("slope", 3, "slope of toboggan")
+	flagSlopeInput = flag.String("slope-input", "slope-input-1.txt", "input file with slopes for toboggan")
 )
 
 func main() {
 	flag.Parse()
 
-	file, err := os.Open(os.Args[len(os.Args)-1])
+	fieldFile, err := os.Open(os.Args[len(os.Args)-1])
 	if err != nil {
 		log.Fatalf("error opening input file: %v", err)
 	}
-	defer file.Close()
+	defer fieldFile.Close()
 
-	f, err := parseFieldFromFile(file)
+	f, err := parseFieldFromFile(fieldFile)
 	if err != nil {
-		log.Fatalf("error parsing input field as field: %v", err)
+		log.Fatalf("error parsing input file as field: %v", err)
 	}
 
-	cnt := f.treeCount(3)
-	fmt.Printf("encountered %d trees on the path with slope %d\n", cnt, *flagSlope)
+	slopeFile, err := os.Open(*flagSlopeInput)
+	if err != nil {
+		log.Fatalf("error opening slope input file: %v", err)
+	}
+	defer slopeFile.Close()
+
+	slopes, err := parseSlopesFromFile(slopeFile)
+	if err != nil {
+		log.Fatalf("error parsing slope file: %v", err)
+	}
+	cnts := []int{}
+	for _, s := range slopes {
+		cnt := f.treeCount(int(s.x), int(s.y))
+		log.Printf("encountered %d trees on the path with slope %d,%d\n", cnt, s.x, s.y)
+		cnts = append(cnts, cnt)
+	}
+
+	log.Printf("product of tree counts from all slopes: %d", product(cnts))
 }
 
 func parseFieldFromFile(file *os.File) (field, error) {
@@ -56,14 +78,46 @@ func parseFieldFromFile(file *os.File) (field, error) {
 	return f, nil
 }
 
-func (f *field) treeCount(slope int) int {
+func parseSlopesFromFile(file *os.File) ([]slope, error) {
+	slopes := []slope{}
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		slopeParts := strings.Split(s.Text(), ",")
+		if len(slopeParts) != 2 {
+			return nil, fmt.Errorf("unexpected number of slope parts %d", len(slopeParts))
+		}
+		x, err := strconv.ParseInt(slopeParts[0], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("could not parse slope x part as int: %v", err)
+		}
+		y, err := strconv.ParseInt(slopeParts[1], 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("could not parse slope y part as int: %v", err)
+		}
+		slopes = append(slopes, slope{x, y})
+	}
+	return slopes, nil
+}
+
+func (f *field) treeCount(slopeX, slopeY int) int {
 	cnt := 0
 	x := 0
-	for _, r := range *f {
+	for y, r := range *f {
+		if y%slopeY != 0 {
+			continue
+		}
 		if r[x%len(r)] {
 			cnt++
 		}
-		x = x + 3
+		x = x + slopeX
 	}
 	return cnt
+}
+
+func product(nums []int) int {
+	ret := 1
+	for _, i := range nums {
+		ret *= i
+	}
+	return ret
 }
