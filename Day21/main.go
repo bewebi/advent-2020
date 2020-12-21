59807c2833dfcc8047d44f98d19d53c6c3d6154a
Bernie Birnbaum <bewebi@earthlink.net>
2020-12-21 00:45:32 -0500

    Day 21 part 2

--- Day21/main.go
+++ Day21/main.go
@@ -5,12 +5,15 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"sort"
 	"strings"
 )
 
 var (
 	ingredientsRE = regexp.MustCompile(`^([a-z ]+) \(contains ([a-z, ]+)\)$`)
 
+	allergenList          = []string{}
+	ingredientList        = []string{}
 	allergenPossabilities = map[string][]string{}
 	suspectedIngredients  = map[string][]string{}
 	ingredientCount       = map[string]int{}
@@ -66,7 +69,34 @@ func main() {
 			nonAllergenCount += ingredientCount[ingredient]
 		}
 	}
-	log.Printf("ingredients without allergens appear %d times", nonAllergenCount)
+	log.Printf("ingredients that definitely have allergens appear %d times", nonAllergenCount)
+
+	for {
+		done := true
+		for allergen, possabilities := range allergenPossabilities {
+			if len(possabilities) == 1 {
+				for a, ps := range allergenPossabilities {
+					if a != allergen {
+						allergenPossabilities[a] = removeString(possabilities[0], ps)
+					}
+				}
+			} else {
+				done = false
+			}
+		}
+		if done {
+			break
+		}
+	}
+	for allergen := range allergenPossabilities {
+		allergenList = append(allergenList, allergen)
+	}
+	sort.Strings(allergenList)
+
+	for _, allergen := range allergenList {
+		ingredientList = append(ingredientList, allergenPossabilities[allergen][0])
+	}
+	log.Printf("list of allergenic ingredients alphabetical by allergen: %s", strings.Join(ingredientList, ","))
 }
 
 func inStrings(s string, ss []string) bool {
