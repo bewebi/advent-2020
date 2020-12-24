b36d81d052981b94938f699179b5f8bb566e60e8
Bernie Birnbaum <bewebi@earthlink.net>
2020-12-24 01:32:10 -0500

    Day 24 part 2

--- Day24/main.go
+++ Day24/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"log"
 	"os"
 )
 
+var (
+	flagDays     = flag.Int("days", 100, "number of days to flip")
+	flagMaxTiles = flag.Int("max-tiles", 2000, "maximum size of tile grid")
+)
+
 func main() {
+	flag.Parse()
+
 	f, err := os.Open(os.Args[len(os.Args)-1])
 	if err != nil {
 		log.Fatalf("error opening input file: %v", err)
@@ -15,11 +23,14 @@ func main() {
 
 	s := bufio.NewScanner(f)
 
-	tiles := map[int]map[int]bool{}
+	tiles := make([][]bool, *flagMaxTiles)
+	for row := range tiles {
+		tiles[row] = make([]bool, *flagMaxTiles)
+	}
 
 	for s.Scan() {
 		steps := s.Text()
-		curX, curY := 0, 0
+		curX, curY := 1000, 1000
 		for steps != "" {
 			nonCardinal := len(steps) >= 2
 			if nonCardinal {
@@ -55,19 +66,64 @@ func main() {
 				steps = steps[1:]
 			}
 		}
-		if _, ok := tiles[curX]; !ok {
-			tiles[curX] = map[int]bool{}
-		}
+
 		tiles[curX][curY] = !tiles[curX][curY]
 	}
-	blackCount, total := 0, 0
+	blackCount := 0
 	for _, row := range tiles {
 		for _, tile := range row {
-			total++
 			if tile {
 				blackCount++
 			}
 		}
 	}
-	log.Printf("after all moves there are %d black tiles of %d total", blackCount, total)
+	log.Printf("after all moves there are %d black tiles", blackCount)
+
+	for i := 0; i < *flagDays; i++ {
+		tiles = dailyFlip(tiles)
+	}
+	blackCount = 0
+	for _, row := range tiles {
+		for _, tile := range row {
+			if tile {
+				blackCount++
+			}
+		}
+	}
+	log.Printf("after %d days of flipping there are %d black tiles", *flagDays, blackCount)
+}
+
+func dailyFlip(tiles [][]bool) [][]bool {
+	nextTiles := make([][]bool, *flagMaxTiles)
+	for row := range nextTiles {
+		nextTiles[row] = make([]bool, *flagMaxTiles)
+	}
+
+	for x := 1; x < len(tiles)-1; x++ {
+		for y := 2; y < len(tiles[0])-2; y++ {
+			blackCount := 0
+			if tiles[x-1][y-1] {
+				blackCount++
+			}
+			if tiles[x-1][y+1] {
+				blackCount++
+			}
+			if tiles[x][y-2] {
+				blackCount++
+			}
+			if tiles[x][y+2] {
+				blackCount++
+			}
+			if tiles[x+1][y-1] {
+				blackCount++
+			}
+			if tiles[x+1][y+1] {
+				blackCount++
+			}
+			if (tiles[x][y] && (blackCount == 1 || blackCount == 2)) || (!tiles[x][y] && blackCount == 2) {
+				nextTiles[x][y] = true
+			}
+		}
+	}
+	return nextTiles
 }
